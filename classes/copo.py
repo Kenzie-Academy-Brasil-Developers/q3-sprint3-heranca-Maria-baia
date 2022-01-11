f27e33555fb41bbe120ec65c907d55abafa887fe
Maria <mfb171201@gmail.com>
2022-01-11 11:09:38 -0400

    entrega feita

--- classes/copo.py
+++ classes/copo.py
@@ -1,1 +1,36 @@
-# Desenvolva sua classe Copo aqui.
+from classes.recipiente import Recipiente
+
+class Copo(Recipiente):
+    def __init__(self, tamanho):
+        super().__init__(tamanho)
+        self.tamanho = tamanho
+    
+    def encher(self, bebida = 'água'):
+        if(self.limpo == False):
+            return 'Não se pode encher um copo sujo'
+        self.limpo = False
+        self.bebida = bebida
+        self.conteudo = self.tamanho
+    
+    def beber(self, quantidade):
+        if(quantidade < 0):
+            return 'Quantidade deve ser positiva'
+        elif(quantidade > self.conteudo):
+            return 'Não há bebida suficiente no copo'
+        else:
+            self.conteudo = self.conteudo - quantidade
+    
+    def lavar(self):
+        self.bebida = None
+        self.conteudo = 0
+        self.limpo = True
+
+    def __str__(self):
+        if(self.conteudo <= 0):
+            return f'Um copo vazio de {float(self.tamanho)}ml'
+        return f'Um copo de {float(self.tamanho)}ml contendo {float(self.conteudo)}ml de {self.bebida}'
+    
+    def __repr__(self):
+        if(self.conteudo <= 0):
+            return f'Um copo vazio de {float(self.tamanho)}ml'
+        return f'Um copo de {float(self.tamanho)}ml contendo {float(self.conteudo)}ml de {self.bebida}'
--- classes/recipiente.py
+++ classes/recipiente.py
@@ -1,1 +1,39 @@
-# Desenvolva sua classe Recipiente aqui.
+class Recipiente:
+    def __init__(self, tamanho=0, conteudo: float=0, limpo=True):
+        if(tamanho < 0):
+            self.tamanho = 0
+        else:
+            self.tamanho = tamanho
+        self.conteudo = conteudo
+        self.limpo = limpo
+    
+    def esvaziar(self):
+        self.conteudo = 0
+    
+    def esta_vazio(self):
+        if(self.conteudo <= 0):
+            return True
+        return False
+
+    def lavar(self): 
+        self.conteudo = 0
+        self.limpo = True
+    
+    def esta_limpo(self):
+        if(self.limpo == True):
+            return True
+        return False
+
+    def estado(self):
+        if(self.limpo == True):
+            return "limpo"
+        return "sujo"
+
+    def sujar(self):
+        self.limpo = False
+
+    def __str__(self):
+        return f'Um recipiente {self.estado()} não especificado'
+    
+    def __repr__(self):
+        return f'Um recipiente {self.estado()} não especificado'
